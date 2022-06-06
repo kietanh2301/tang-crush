39d6f14eee68f3f339003adbb805fc24fbca230b
kietanh2301 <106978551+kietanh2301@users.noreply.github.com>
2022-06-06 07:34:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bae à!;-;;',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    Lạc vào tim 2:>`,
+    btnIntro: 'ehe:33',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên ;-;?',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy:< ',
+    btnYes: 'Đúng z iu 2>:33',
+    btnNo: 'Ko Anh 2 à :>',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh :3',
+    btnReply: 'Gửi cho 2 >:3',
+    reply: 'Bởi vì e iu 2 nhất:33 ',
+    mess: '2 bik mà 🥰. Iu em nhiều vaiz 😘😘',
+    messDesc: 'Đến lúc gặp đc đói kêu 2 mua cho e ăn nha bae:33.',
+    btnAccept: 'Okiiiii lun 2 iu :3',
+    messLink: 'https://www.facebook.com/profile.php?id=100073236192720' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
